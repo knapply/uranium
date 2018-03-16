b01d5bd169315387568e0df373253945ff9788c5
syknapptic <doc.synaptogenesis@gmail.com>
2018-03-16 16:48:27 -0700

    transfer and update functions

--- R/plot_planet_rgb.R
+++ R/plot_planet_rgb.R
@@ -1,5 +1,4 @@
-plot_planet_rgb <- function(raster_brick, 
-                            red = 3, green = 2, blue = 1,
+plot_planet_rgb <- function(raster_brick, red = 3, green = 2, blue = 1, 
                             stretch = "lin", ...){
   plotRGB(raster_brick, r = red, g = green, b = blue, tck = 0,
           stretch = stretch, ...)
