2c39522078c02fe54a042b577afbc93df0c5885c
syknapptic <doc.synaptogenesis@gmail.com>
2018-03-16 17:54:55 -0700

    the error messages I wish others would use

--- R/planet_utils.R
+++ R/planet_utils.R
@@ -25,7 +25,9 @@ plot_planet_rgb <- function(rast_brick, red = 3, green = 2, blue = 1, stretch = 
 clean_dirs <- function(directory, SR_or_MS){
   if(!str_to_upper(SR_or_MS) %in% c("SR", "MS")){
     stop("Dude, it says `SR` or `MS`. Get it together.")
+    
   } else SR_or_MS <- str_extract(str_to_upper(SR_or_MS), "SR|MS")
+  
   sr_count <- directory %>% 
     dir(recursive = TRUE, full.names = TRUE) %>% 
     str_count(paste0(SR_or_MS, "\\.tif$")) %>% 
